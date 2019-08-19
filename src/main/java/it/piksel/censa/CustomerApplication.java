b1e9bc5d1e501cd47b9a032715c9f7fe29a75fce
andrea.censabella <andrea.censabella@piksel.com>
2019-08-19 15:09:20 +0200

    aggiunta valorizzazione createdDate and modifiedDate

--- src/main/java/it/piksel/censa/CustomerApplication.java
+++ src/main/java/it/piksel/censa/CustomerApplication.java
@@ -7,9 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableMongoAuditing
 public class CustomerApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
